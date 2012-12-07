bff048fec9379c858afaa9a4d47f9151752a662c
zsx1986 <rain19860806@126.com>
2012-12-07 10:37:25 +0800

    Just only create a single process
    this application just only runs as a single process.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -15,7 +15,16 @@ namespace HelloWorld
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            bool IsCreateNew = true;
+            //System.Threading.Mutex mutex = new System.Threading.Mutex(true, "HelloWorld", out IsRunning);
+            //System.Threading.Mutex mutex = new System.Threading.Mutex(true, Application.ProductName, out IsCreateNew);
+            System.Threading.Mutex mutex = new System.Threading.Mutex(true, System.Diagnostics.Process.GetCurrentProcess().ProcessName, out IsCreateNew);
+
+            if (!IsCreateNew)
+                MessageBox.Show("Application is running!", "Tip", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+                Application.Run(new Form1());
         }
     }
 }
